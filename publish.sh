ac5fabd9b61c2db5cb1bf8eb9bae49ed437f9361
al <cangzan@gmail.com>
2021-08-27 11:31:01 +0800

    change: build test script.

--- publish.sh
+++ publish.sh
@@ -5,22 +5,30 @@ args="${@}"
 npm=$(command -v npm)
 workDir=$(pwd)
 
-declare -a arr=("op.gg" "op.gg-aram" "murderbridge" "lolalytics" "lolalytics-aram")
-
-./data-crawler $args
-
 publish() {
-  local dir=$0
-  echo "processing $dir"
+  local dir=$1
+  log "processing $dir"
 
   if [ -d "$workDir/output/$dir" ]; then
     cp "$workDir/output/index.json" "$workDir/output/$dir/"
     cd "$workDir/output/$dir" || return
     $npm publish --access public
   else
-    echo "$workDir/output/$dir not exists"
+    log "$workDir/output/$dir not exists"
   fi
 }
+
+log() {
+  echo "[publish] $1"
+}
+
+log "pwd is $workDir"
+
+declare -a arr=("op.gg" "op.gg-aram" "murderbridge" "lolalytics" "lolalytics-aram")
+
+./data-crawler $args
+
+cd "$workDir" || return
 
 for i in "${arr[@]}"; do
   publish "$i"
